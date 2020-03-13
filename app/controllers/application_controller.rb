2b2cd758c81a65a18cd413e08b27e61dcd1900dd
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-03-14 00:56:16 +0700

    add scaffold cart and items ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  private
+  
+  def current_cart
+    Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 	serialize :image, JSON # If you use SQLite, add this line.
 	belongs_to :user, optional: true
+	has_many :line_items, dependent: :destroy
+	before_destroy :check_if_has_line_item
 
 	validates :title, :price, :model, presence: true
 	validates :description, length: { maximum: 1000, too_long: "%{count} characters is 	the maximum aloud. "}
@@ -11,5 +13,13 @@ class Product < ApplicationRecord
 	MODEL = %w{ MacBook Iphone AppleWatch Ipad }
 	COLOR = %w{ Black White Navy Blue Red Clear Satin Yellow }
 	CONDITION = %w{ New Used }
+
+	def check_if_has_line_item
+		unless line_items.empty?
+			errors.add(:base, 'This product has a LineItem')
+			throw :abort
+		end
+	end
+		
 
 end
